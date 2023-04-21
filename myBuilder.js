05614987f8ab92ece1be7045ef5b2379d79184a5
十二少 <1484744996@qq.com>
2023-04-21 14:50:22 +0800

    解决错误推送BUG

--- myBuilder.js
+++ myBuilder.js
@@ -6,6 +6,7 @@ let cfStr = fs.readFileSync(path, { encoding: 'utf-8' })
 
 const config = JSON.parse(cfStr)
 
+config.baseUrl = "http://192.168.1.201:80"
 config.map.sourceUrl = ''
 
 fs.writeFile(path, JSON.stringify(config), { encoding: 'utf-8', flag: 'w' }, () => { })
